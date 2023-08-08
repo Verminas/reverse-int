bbd767d9d26228aaa38f34cd248296d08681a4a2
verminas <veremeeva96@gmail.com>
2023-08-08 13:44:54 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) n = -n;
+    let arrayOfString = Array.from(String(n));
+    let arrReverse = arrayOfString.reverse();
+    let str = arrReverse.join('');
+    n = +str;
+    return n;
 }
